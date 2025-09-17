09b40636af2edc8decc6371bc7e46c4ed4283109
y_izmaylova <y.izmaylova@tanais.ru>
2025-09-17 13:07:41 +0300

    feat: update to 25.2.0.40

--- tanais.IntCBR/tanais.IntCBR.ClientBase/ModuleClientFunctions.cs
+++ tanais.IntCBR/tanais.IntCBR.ClientBase/ModuleClientFunctions.cs
@@ -6,7 +6,7 @@ using Sungero.CoreEntities;
 
 namespace Tanais.IntCBR.Client
 {
-  public class ModuleFunctions
+  public partial class ModuleFunctions
   {
 
   }
--- tanais.IntCBR/tanais.IntCBR.Server/ModuleServerFunctions.cs
+++ tanais.IntCBR/tanais.IntCBR.Server/ModuleServerFunctions.cs
@@ -6,7 +6,7 @@ using Sungero.CoreEntities;
 
 namespace Tanais.IntCBR.Server
 {
-  public class ModuleFunctions
+  public partial class ModuleFunctions
   {
 
   }
